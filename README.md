e492f8bbc6f88845a0e7baee0926a2cba764920b
Noahkiq <noahkiqgaming@gmail.com>
2017-01-21 14:56:38 -0500

    Initial bots

---
+++ README.md
@@ -0,0 +1,6 @@
+# HTC-Collection
+Collection of bots made for/used on [HTwins Central](https://discord.gg/011z1ORKMpEtNwD7b).
+
+## Credits
+joinbot - myself
+[musicbot](https://github.com/Just-Some-Bots/MusicBot) - [various contributors](https://github.com/Just-Some-Bots/MusicBot/graphs/contributors) (including myself for the !forceskip code)
---
+++ joinbot/joinbot/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Discord;
+
+namespace joinbot
+{
+	class joinbot
+	{
+		static void Main(string[] args) => new joinbot().Start();
+
+		private DiscordClient _client;
+
+		public void Start()
+		{
+			_client = new DiscordClient();
+
+			_client.Log.Message += (s, e) => Console.WriteLine($"[{e.Severity}] {e.Source}: {e.Message}");
+
+			// Register a Hook into the UserJoined event using a Lambda
+			_client.UserJoined += async (s, e) => {
+				Channel logChannel = e.Server.GetChannel(207659596167249920);
+				if(logChannel != null)
+					await logChannel.SendMessage($"{e.User.Name} ({e.User.Id}) joined the server.");
+			};
+
+			// Register a Hook into the UserUnanned event using a Lambda
+			_client.UserLeft += async (s, e) => {
+				Channel logChannel = e.Server.GetChannel(207659596167249920);
+				if (logChannel != null)
+					await logChannel.SendMessage($"{e.User.Name} ({e.User.Id}) left the server.");
+			};
+
+			string token = File.ReadAllText("token.txt");
+			_client.ExecuteAndWait(async () => {
+				await _client.Connect(token, TokenType.Bot);
+				Console.WriteLine($"Connected as {_client.CurrentUser.Name}#{_client.CurrentUser.Discriminator}");
+			});
+
+		}
+	}
+
+}
