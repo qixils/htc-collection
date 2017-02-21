2aef326826cc968e6b555ff23c9dca8cbf823577
Noahkiq <noahkiqgaming@gmail.com>
2017-02-21 14:29:05 -0500

    [Flair] Better logs, crash prevention
    Discord.Net warning logs now have timestamps and the bot should now no longer crash if it encounters an error.

--- flairbot/flairbot/Program.cs
+++ flairbot/flairbot/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +14,7 @@ class Program
 
 	public void Start()
 	{
-		_client.Log.Message += (s, e) => Console.WriteLine($"[{e.Severity}] {e.Source}: {e.Message}");
+		_client.Log.Message += (s, e) => Console.WriteLine($"[{e.Severity} - {DateTime.UtcNow.Hour}:{DateTime.UtcNow.Minute}:{DateTime.UtcNow.Second}] {e.Source}: {e.Message}");
 
 		_client.MessageReceived += async (s, e) =>
 		{
@@ -57,99 +57,112 @@ class Program
 				else
 					Console.WriteLine("The bot currently has permission to manage roles.");
 			}
-			else if (e.Channel.IsPrivate && inHTC)
-			{
-				Server htc = _client.GetServer(184755239952318464);
-				User user = htc.GetUser(e.User.Id);
-				List<Role> teams = new List<Role>();
-				var roles = htc.Roles;
+            try
+            {
+                if (e.Channel.IsPrivate && inHTC)
+                {
+                    Server htc = _client.GetServer(184755239952318464);
+                    User user = htc.GetUser(e.User.Id);
+                    List<Role> teams = new List<Role>();
+                    var roles = htc.Roles;
 
-				foreach (Role role in roles)
-				    if (role.Name.StartsWith("#Team"))
-				        teams.Add(role);
+                    foreach (Role role in roles)
+                        if (role.Name.StartsWith("#Team"))
+                            teams.Add(role);
 
-				if (e.Message.Text.ToLower() == "!remove")
-				{
-					var removed = false;
-					foreach (Role role in teams)
-					{
-						if(user.HasRole(role)) {
-							removed = true;
-						    await user.RemoveRoles(role);
-						    System.Threading.Thread.Sleep(500);
-						}
-					}
-					if (removed)
-					    await e.Channel.SendMessage("You have successfully been removed from your team.");
-					else
-					    await e.Channel.SendMessage("You're not on a team!");
-				}
-				else if (e.Message.Text.ToLower() == "!joho")
-				{
-					foreach (Role role in teams)
-					{
-						if(user.HasRole(role)) {
-						    await user.RemoveRoles(role);
-						    System.Threading.Thread.Sleep(500);
-						}
-					}
-					Role team = htc.FindRoles("#TeamJoho", false).FirstOrDefault();
-					await user.AddRoles(team);
-					await e.Channel.SendMessage("You have joined #TeamJoho.");
-				}
-				else if (e.Message.Text.ToLower() == "!meester")
-				{
-					foreach (Role role in teams)
-					{
-						if(user.HasRole(role)) {
-						    await user.RemoveRoles(role);
-						    System.Threading.Thread.Sleep(500);
-						}
-					}
-					Role team = htc.FindRoles("#TeamMeester", false).FirstOrDefault();
-					await user.AddRoles(team);
-					await e.Channel.SendMessage("You have joined #TeamMeester.");
-				}
-				else if (e.Message.Text.ToLower() == "!midge")
-				{
-					foreach (Role role in teams)
-					{
-						if(user.HasRole(role)) {
-						    await user.RemoveRoles(role);
-						    System.Threading.Thread.Sleep(500);
-						}
-					}
-					Role team = htc.FindRoles("#TeamMidge", false).FirstOrDefault();
-					await user.AddRoles(team);
-					await e.Channel.SendMessage("You have joined #TeamMidge.");
-				}
-				else if (e.Message.Text.ToLower() == "!midnight")
-				{
-					foreach (Role role in teams)
-					{
-						if(user.HasRole(role)) {
-						    await user.RemoveRoles(role);
-						    System.Threading.Thread.Sleep(500);
-						}
-					}
-					Role team = htc.FindRoles("#TeamMidnight", false).FirstOrDefault();
-					await user.AddRoles(team);
-					await e.Channel.SendMessage("You have joined #TeamMidnight.");
-				}
-				else if (e.Message.Text.ToLower() == "!yessoan")
-				{
-					foreach (Role role in teams)
-					{
-						if(user.HasRole(role)) {
-						    await user.RemoveRoles(role);
-						    System.Threading.Thread.Sleep(500);
-						}
-					}
-					Role team = htc.FindRoles("#TeamYessoan", false).FirstOrDefault();
-					await user.AddRoles(team);
-					await e.Channel.SendMessage("You have joined #TeamYessoan.");
-				}
-			}
+                    if (e.Message.Text.ToLower() == "!remove")
+                    {
+                        var removed = false;
+                        foreach (Role role in teams)
+                        {
+                            if (user.HasRole(role))
+                            {
+                                removed = true;
+                                await user.RemoveRoles(role);
+                                System.Threading.Thread.Sleep(500);
+                            }
+                        }
+                        if (removed)
+                            await e.Channel.SendMessage("You have successfully been removed from your team.");
+                        else
+                            await e.Channel.SendMessage("You're not on a team!");
+                    }
+                    else if (e.Message.Text.ToLower() == "!joho")
+                    {
+                        foreach (Role role in teams)
+                        {
+                            if (user.HasRole(role))
+                            {
+                                await user.RemoveRoles(role);
+                                System.Threading.Thread.Sleep(500);
+                            }
+                        }
+                        Role team = htc.FindRoles("#TeamJoho", false).FirstOrDefault();
+                        await user.AddRoles(team);
+                        await e.Channel.SendMessage("You have joined #TeamJoho.");
+                    }
+                    else if (e.Message.Text.ToLower() == "!meester")
+                    {
+                        foreach (Role role in teams)
+                        {
+                            if (user.HasRole(role))
+                            {
+                                await user.RemoveRoles(role);
+                                System.Threading.Thread.Sleep(500);
+                            }
+                        }
+                        Role team = htc.FindRoles("#TeamMeester", false).FirstOrDefault();
+                        await user.AddRoles(team);
+                        await e.Channel.SendMessage("You have joined #TeamMeester.");
+                    }
+                    else if (e.Message.Text.ToLower() == "!midge")
+                    {
+                        foreach (Role role in teams)
+                        {
+                            if (user.HasRole(role))
+                            {
+                                await user.RemoveRoles(role);
+                                System.Threading.Thread.Sleep(500);
+                            }
+                        }
+                        Role team = htc.FindRoles("#TeamMidge", false).FirstOrDefault();
+                        await user.AddRoles(team);
+                        await e.Channel.SendMessage("You have joined #TeamMidge.");
+                    }
+                    else if (e.Message.Text.ToLower() == "!midnight")
+                    {
+                        foreach (Role role in teams)
+                        {
+                            if (user.HasRole(role))
+                            {
+                                await user.RemoveRoles(role);
+                                System.Threading.Thread.Sleep(500);
+                            }
+                        }
+                        Role team = htc.FindRoles("#TeamMidnight", false).FirstOrDefault();
+                        await user.AddRoles(team);
+                        await e.Channel.SendMessage("You have joined #TeamMidnight.");
+                    }
+                    else if (e.Message.Text.ToLower() == "!yessoan")
+                    {
+                        foreach (Role role in teams)
+                        {
+                            if (user.HasRole(role))
+                            {
+                                await user.RemoveRoles(role);
+                                System.Threading.Thread.Sleep(500);
+                            }
+                        }
+                        Role team = htc.FindRoles("#TeamYessoan", false).FirstOrDefault();
+                        await user.AddRoles(team);
+                        await e.Channel.SendMessage("You have joined #TeamYessoan.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[ERROR] A bot-crashing error occured somewhere in the code. Details: \n" + ex.ToString());
+            }
 
 		};
 
